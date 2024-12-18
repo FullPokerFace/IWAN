417ff1fde33af365b5e7b6f73e6275ec38014163
VardanMelkumyan <trilodi@gmail.com>
2024-12-17 20:08:33 -0500

    added slashed to urls

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="privacy" element={<PrivacyPolicy />} />
-          <Route path="terms" element={<TermsOfUse />} />
-          <Route path="convert-monkey" element={<ConvertMonkey />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<TermsOfUse />} />
+          <Route path="/convert-monkey" element={<ConvertMonkey />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
